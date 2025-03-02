a7e78ec88271b6c76e2027ecdf77de86458386f7
Kishan Prajapati <kmprajapati1910@example.com>
2025-03-02 12:01:35 +0530

    Url set

--- client/src/components/layouts/SideDrawer.jsx
+++ client/src/components/layouts/SideDrawer.jsx
@@ -34,7 +34,7 @@ const SideDrawer = ({ drawerOpen, toggleDrawer, onChatAccess }) => {
     try {
       setChatCreateLoading(true)
       const { data } = await axiosInstance.post("/api/chat", { userId });
-      if (!chats.some((chat) => chat._id === data._id)) {
+      if (!chats.some((chat) => chat._id === data?._id)) {
         setChats([data, ...chats]); // Add to context state
       }
       setSelectedChat(data)
@@ -46,10 +46,9 @@ const SideDrawer = ({ drawerOpen, toggleDrawer, onChatAccess }) => {
       setChatCreateLoading(false)
     }
   };
-
   return (
     <div
-      className={`fixed top-0 left-0 z-50 w-1/4 h-full bg-zinc-100 text-zinc-800 shadow-lg transform transition-transform ${
+      className={`fixed top-0 left-0 z-50 w-4/5 sm:w-1/2 md:w-1/3 lg:w-1/4 h-full bg-zinc-100 text-zinc-800 shadow-lg transform transition-transform ${
         drawerOpen ? "translate-x-0" : "-translate-x-full"
       }`}
     >
@@ -62,42 +61,43 @@ const SideDrawer = ({ drawerOpen, toggleDrawer, onChatAccess }) => {
           <RxCross2 className="text-xl" />
         </button>
       </div>
-      <div className="p-4 max-w-md mx-auto">
+  
+      <div className="p-4 max-w-1/2">
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <IoSearch className="inline text-sky-500 text-lg" />
+            <IoSearch className="inline text-sky- text-lg" />
           </div>
           <input
             type="search"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             id="default-search"
-            className="block w-full p-4 pl-10 text-sm text-gray-900 outline-none border  rounded-lg bg-gray-50   border-gray-300 placeholder-gray-400   "
+            className="block w-full p-3 sm:p-4 pl-10 text-sm text-gray-900 outline-none border rounded-lg bg-gray-50 border-gray-300 placeholder-gray-400"
             placeholder="Search users..."
             required
           />
           <button
             onClick={handleSearch}
-            className="text-white absolute right-2.5 bottom-2.5 bg-zinc-800 hover:bg-zinc-700 transition ease outline-none rounded-lg text-sm px-4 py-2 "
+            className="text-white absolute right-2.5 bottom-2 bg-zinc-800 hover:bg-zinc-700 transition ease outline-none rounded-lg text-sm px-3 sm:px-4 py-2"
           >
             Go
           </button>
         </div>
-
+  
         {searchLoading ? (
-          <SearchLoader /> // Loader component for search results
+          <SearchLoader />
         ) : searchResults.length > 0 ? (
-          <ul className="w-full divide-y mt-2 divide-zinc-300 ">
+          <ul className="w-full divide-y mt-2 divide-zinc-300">
             {searchResults.map((user, index) => (
               <li
                 key={index}
                 className="pb-3 px-2 pt-3 sm:pb-4 cursor-pointer hover:bg-sky-100"
-                onClick={(e) => handleUserClick(e ,user._id)}
+                onClick={(e) => handleUserClick(e, user._id)}
               >
-                <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                <div className="flex items-center space-x-3 sm:space-x-4">
                   <div className="flex-shrink-0">
                     <img
-                      className="w-8 h-8 rounded-full"
+                      className="w-10 h-10 sm:w-8 sm:h-8 rounded-full"
                       src={
                         `${basePicUrl}/${user?.pic}` ||
                         `${basePicUrl}/default-avatar.png`
@@ -106,10 +106,10 @@ const SideDrawer = ({ drawerOpen, toggleDrawer, onChatAccess }) => {
                     />
                   </div>
                   <div className="flex-1 min-w-0">
-                    <p className="text-sm font-medium text-zinc-700 ">
+                    <p className="text-sm sm:text-base font-medium text-zinc-700">
                       {user.name}
                     </p>
-                    <p className="text-sm truncate text-zinc-400">
+                    <p className="text-xs sm:text-sm truncate text-zinc-400">
                       {user.email}
                     </p>
                   </div>
@@ -118,11 +118,10 @@ const SideDrawer = ({ drawerOpen, toggleDrawer, onChatAccess }) => {
             ))}
           </ul>
         ) : (
-          <p className="mt-4 text-gray-400">No users found</p>
+          <p className="mt-4 text-gray-400 text-center">No users found</p>
         )}
       </div>
     </div>
   );
-};
-
-export default SideDrawer;
+}
+export default SideDrawer
